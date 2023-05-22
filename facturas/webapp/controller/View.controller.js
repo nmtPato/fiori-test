a30b61d1dcdb1015f5e3a1c1c286145ec24c7cba
nmtPato <nmt.pato@gmail.com>
2023-05-22 20:08:39 +0000

    cambios

--- facturas/webapp/controller/View.controller.js
+++ facturas/webapp/controller/View.controller.js
@@ -9,7 +9,23 @@ sap.ui.define([
 
         return Controller.extend("facturasApp.facturas.controller.View", {
             onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("../model/SelectionScreenMenu.json")
+                oView.setModel(oJSONModel, "selectionScreen")
+            },
 
+            onFilter: function(oEvent){
+                console.log(oEvent);
+                // alert("filtrando...")
+            },
+
+            onClearFilter: function(){
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "")
+                oModelSelScreen.setProperty("/CountryKey", "")
+                console.log(oModelSelScreen)
             }
+            
         });
     });
